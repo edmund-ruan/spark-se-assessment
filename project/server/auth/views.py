1d63d6e9fa55858706491a4317d338da611df8fa
Edmund <eshu.ruan@gmail.com>
2022-02-03 18:40:03 -0500

    4616

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -6,6 +6,7 @@ from project.server.models import User
 
 auth_blueprint = Blueprint('auth', __name__)
 
+
 class RegisterAPI(MethodView):
     """
     User Registration Resource
@@ -20,38 +21,41 @@ class RegisterAPI(MethodView):
 
     def post(self):
         # get the post data
-        post_data = request.get_json(); print(request)
+        post_data = request.get_json()
+        print(request)
         # check if user already exists
         user = User.query.filter_by(email=post_data.get('email')).first()
         if not user:
             try:
-                #print('gothere1')
+                # print('gothere1')
                 user = User(
                     email=post_data.get('email'),
                     password=post_data.get('password')
                 )
-                #print('gothere2')
+                # print('gothere2')
                 # insert the user
                 db.session.add(user)
                 db.session.commit()
                 # generate the auth token
-                #print('gothere3')
+                # print('gothere3')
                 auth_token = user.encode_auth_token(user.id)
-                #print(auth_token)
-                #print('gothere4')
+                print(auth_token)
+                # print('gothere4')
+                
                 responseObject = {
                     'status': 'success',
                     'message': 'Successfully registered.',
-                    'auth_token': auth_token.decode()
+                    'auth_token': auth_token
                 }
-                #print('done')
+                # print('done')
                 return make_response(jsonify(responseObject)), 201
             except Exception as e:
+                print(e)
                 responseObject = {
                     'status': 'fail',
                     'message': 'Some error occurred. Please try again.'
                 }
-                #print('exceptuion')
+                # print('exceptuion')
                 return make_response(jsonify(responseObject)), 401
         else:
             responseObject = {
@@ -59,7 +63,6 @@ class RegisterAPI(MethodView):
                 'message': 'User already exists. Please Log in.',
             }
             return make_response(jsonify(responseObject)), 202
-
 
 
 # define the API resources
@@ -71,11 +74,6 @@ auth_blueprint.add_url_rule(
     view_func=registration_view,
     methods=['POST', 'GET']
 )
-
-
-
-
-
 
 
 # code for my registered users
