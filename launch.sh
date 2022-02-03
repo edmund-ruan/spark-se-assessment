1d308493be1972a41add1740f74f0e25bc97997c
Edmund <eshu.ruan@gmail.com>
2022-02-03 16:13:04 -0500

    413

--- launch.sh
+++ launch.sh
@@ -2,6 +2,7 @@
 
 export FLASK_APP=project.server
 export APP_SETTINGS="project.server.config.DevelopmentConfig"
+python from project.server.__init__ import db
 flask db init
 flask db migrate
 flask db upgrade
