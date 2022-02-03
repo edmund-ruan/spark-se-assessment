ea46a6a99f8804e57d3e83b17bdc8935742f4f60
Edmund <eshu.ruan@gmail.com>
2022-02-03 18:18:45 -0500

    4616

--- launch.sh
+++ launch.sh
@@ -3,10 +3,10 @@
 export FLASK_APP=project.server
 export APP_SETTINGS="project.server.config.DevelopmentConfig"
 
-export APP_PORT=${PORT:-5000}
+#export APP_PORT=${PORT:-5000}
 
 flask db init
 flask db migrate
 flask db upgrade
-flask run --host=0.0.0.0 --port=APP_PORT
+flask run --host=0.0.0.0 --port=${PORT:-5000}
 #flask run
